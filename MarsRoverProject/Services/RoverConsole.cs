23b90a188c4b8abef1adf6e8895f953cdfeab12f
ahmet bilgic <ahmet_bilgic@yahoo.com>
2021-01-10 15:28:30 +0300

    Handling Input erros

--- MarsRoverProject/Services/RoverConsole.cs
+++ MarsRoverProject/Services/RoverConsole.cs
@@ -1,36 +1,50 @@
 ﻿using MarsRoverProject.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace MarsRoverProject.Services
 {
     public class RoverConsole
     {
-        private const int RoverNumber = 2;
-        private readonly IRoverService _service;
+        private const int RoverNumber = 2;//Number of Rovers
+        private readonly IRoverService _service;//Dependency Injection services
         public RoverConsole(IRoverService service)
         {
             _service = service;
         }
 
+        //Do Rover Actions
         public void Run()
         {
             RoverModel[] RoverModelList = new RoverModel[RoverNumber];
             try
-            {
-                Console.WriteLine("Test Input:");
-                string AreaValuesLine = Console.ReadLine();
+            {               
+                startinput:
+                    Console.WriteLine("Test Input:");
+                    string AreaValuesLine = Console.ReadLine();
 
-                string[] SplittedAreaValues = AreaValuesLine.Split(' ');
+                string[] SplittedAreaValues = AreaValuesLine.Split(' ');               
+
+                if (string.IsNullOrEmpty(SplittedAreaValues.ElementAtOrDefault(0)) || string.IsNullOrEmpty(SplittedAreaValues.ElementAtOrDefault(1)))
+                {
+                    goto startinput;
+                }
+
                 var Width = Convert.ToInt32(SplittedAreaValues[0]);
                 var Height = Convert.ToInt32(SplittedAreaValues[1]);
 
                 for (int i = 0; i < RoverNumber; i++)
-                {
+                {                   
                     string RoverValuesLine = "";
                     RoverValuesLine = Console.ReadLine();
-                    string[] SplittedRoverValues = RoverValuesLine.Split(' ');
+                    string[] SplittedRoverValues = RoverValuesLine.Split(' ');                  
+
+                    if (string.IsNullOrEmpty(SplittedRoverValues.ElementAtOrDefault(0)) || string.IsNullOrEmpty(SplittedRoverValues.ElementAtOrDefault(1)) || string.IsNullOrEmpty(SplittedRoverValues.ElementAtOrDefault(2)))
+                    {
+                        goto startinput;
+                    }
 
                     var PositionX = Convert.ToInt32(SplittedRoverValues[0]);
                     var PositionY = Convert.ToInt32(SplittedRoverValues[1]);
@@ -39,6 +53,11 @@ namespace MarsRoverProject.Services
                     string RoverPathCommandLine = "";
 
                     RoverPathCommandLine = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(RoverPathCommandLine))
+                    {
+                        goto startinput;
+                    }
 
                     RoverModelList[i] = _service.CalculateRoverCoordinates(Width, Height, RoverPathCommandLine, PositionX, PositionY, Direction);
                 }
@@ -54,7 +73,8 @@ namespace MarsRoverProject.Services
             }
             catch (Exception e)
             {
-                throw e;
+                Console.WriteLine(e);
+                
             }
         }
     }
